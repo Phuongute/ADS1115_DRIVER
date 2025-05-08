806fbbff78d193fcfd56e7805f944a8428b47b62
Phuongute <hnter12333@gmail.com>
2025-05-08 14:52:53 +0100

    Nhet vao r gui lai len day

--- Driver_code/ads1115_Driver.c
+++ Driver_code/ads1115_Driver.c
@@ -16,13 +16,42 @@
 
 static struct i2c_client *ads1115_client;
 
-
+static int ads1115_read
 static int ads1115_set_config(u16 config)
 {
     int ret;
     ret = i2c_smbus_write_word_data(ads1115_client, ADS_REG_CONFIG, cpu_to_be16(config));
     if (ret < 0) {
-        pr_err("ADS1115: Failed to write config: %d\n", ret);
+        pr_err("ADS1115: Failed to set config: %d\n", ret);
+        return ret;
+    }
+}
+static int ads1115_read_adc(void)
+{
+    int ret
+    int adcVal
+    ret = i2c_smbus_read_word_data(ads1115_client,ADS_REG_CONV)
+    if (ret < 0) {
+        printk("ADS1115: Failed to read ADC value: %d\n", ret);
+        return ret
+    }
+    adcVal = be16_to_cpu(ret);
+    return (s16)adcVal;
+    
+}
+static int ads1115_set_loThresh(u16 loThresh){
+    int ret
+    ret = i2c_smbus_write_word_data(ads1115_client,ADS_REG_LO_THRESH,cpu_to_be16(loThresh))
+    if (ret < 0) {
+        printk("ADS1115: Failed to set low thresh: %d\n", ret);
+        return ret;
+    }
+}
+static int ads1115_set_hiThresh(u16 hiThresh){
+    int ret
+    ret = i2c_smbus_write_word_data(ads1115_client,ADS_REG_HI_THRESH,cpu_to_be16(hiThresh))
+    if (ret < 0) {
+        printk("ADS1115: Failed to set hi thresh: %d\n", ret);
         return ret;
     }
 }
@@ -30,13 +59,13 @@ static int ads1115_probe(struct i2c_client *client, const struct i2c_device_id *
 {
     int ret;
     u16 conf = 0xC383; // 1100 0011 1000 0011
+    ads1115_client = client;
     ret = i2c_smbus_write_word_data(client, ADS_REG_CONFIG, cpu_to_be16(conf) );
     if (ret < 0) {
-        printk(KERN_ERR "Failed to config ADS1115\n");
+        printk(KERN_ERR "ADS1115: Failed to config \n");
         return ret;
     }
 
-    ads1115_client = client;
 
     // Read data from MPU6050 sensor
     ret = i2c_smbus_read_word_data(client, 0x00);
@@ -45,7 +74,7 @@ static int ads1115_probe(struct i2c_client *client, const struct i2c_device_id *
         return ret;
     }
 
-    printk(KERN_INFO "ADS1115 driver installed\n");
+    printk(KERN_INFO "ADS1115: driver installed\n");
 
     return 0;
 }
@@ -53,7 +82,7 @@ static int ads1115_probe(struct i2c_client *client, const struct i2c_device_id *
 
 static void ads1115_remove(struct i2c_client *client)
 {
-    printk(KERN_INFO "MPU6050 driver removed\n");
+    printk(KERN_INFO "ADS1115: driver removed\n");
 
     // Clean up
  
